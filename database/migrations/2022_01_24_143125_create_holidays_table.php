2ecb8bd0efa25c2e70349b96f6552b16f75f09d0
artemisia000 <misiapierobon@gmail.com>
2022-01-24 16:02:53 +0100

    start

---
+++ database/migrations/2022_01_24_143125_create_holidays_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            $table->string('place',100);
+            $table->string('people_num',20);
+            $table->tinyint('price');
+            $table->date('date_departure');
+            $table->date('date_return');
+            $table->string('fly',40);
+            $table->string('hotel',70);
+            $table->text('description');
+            $table->boolean('available')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holidays');
+    }
+}
